f865bb9ab683ac5df2963278f44ec8312fc16b19
Arshad59 <arshadsheikh521@gmail.com>
2024-05-01 08:56:18 +0530

    add favourite logic

--- app/src/main/java/com/example/newsapp/CustomViewHolder.java
+++ app/src/main/java/com/example/newsapp/CustomViewHolder.java
@@ -13,13 +13,14 @@ public class CustomViewHolder extends RecyclerView.ViewHolder {
     TextView text_title, text_source,content,time;
     ImageView img_headline;
     CardView cardview;
+    ImageView heart;
     public CustomViewHolder(@NonNull View itemView) {
         super(itemView);
         text_title = itemView.findViewById(R.id.text_title);
         content= itemView.findViewById(R.id.content);
         time=itemView.findViewById(R.id.time);
         text_source = itemView.findViewById(R.id.text_source);
-
+        heart = itemView.findViewById(R.id.imageView2);
         img_headline=itemView.findViewById(R.id.img_headline);
 
         cardview=itemView.findViewById(R.id.main_container);
